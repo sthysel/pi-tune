0ae9e863ba90b0a0eab44cdde85c21726101ce10
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2015-11-04 22:47:47 +0800

    Removes cruft

--- diet.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# slim down installation 
-
-sudo apt-get install deborphan
-sudo apt-get autoremove --purge libx11-.* lxde-.* raspberrypi-artwork xkb-data omxplayer penguinspuzzle sgml-base xml-core alsa-.* cifs-.* samba-.* fonts-.* desktop-* gnome-.*
-sudo apt-get autoremove --purge $(deborphan)
-sudo apt-get autoremove --purge
-sudo apt-get autoclean
